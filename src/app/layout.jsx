9f3f2fdcd447790f4eea55b820e7b1f64dd3d0b0
Nyfong <litongfong12@gmail.com>
2024-11-12 10:06:37 +0700

    tweenty five

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,31 @@
 import localFont from "next/font/local";
+import logo from "@/assets/icon/fav.png";
 import "./globals.css";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import UnstickyNav from "@/components/common/UnstickyNav";
+import Head from "next/head";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Your Page Title</title>
+        <meta
+          name="nigga"
+          content="A short description of your page's content"
+        />
+        <meta property="og:title" content="Your Page Title" />
+        <meta
+          property="og:description"
+          content="A detailed description of your page's content"
+        />
+        <meta
+          property="og:image"
+          content="https://wallpapers.com/images/featured/minecraft-s2kxfahyg30sob8q.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body>
         <UnstickyNav />
         <Navbar />
