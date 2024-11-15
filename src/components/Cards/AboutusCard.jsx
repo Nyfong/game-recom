6aa185e69eaa18356b794eb9a0e6bda33bc3ada8
Nyfong <litongfong12@gmail.com>
2024-11-15 16:07:36 +0700

    thrity eight

--- src/components/Cards/AboutusCard.jsx
+++ src/components/Cards/AboutusCard.jsx
@@ -4,6 +4,7 @@ import Vy from "@/assets/yu.png";
 import Kot from "@/assets/kot.png";
 import Image from "next/image";
 import logo from "@/assets/icon/favBlue.png";
+import Link from "next/link";
 let AboutUsCard = () => {
   const pic = [Kaiseng, Nyfong, Vy, Kot];
   return (
@@ -13,28 +14,31 @@ let AboutUsCard = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
-        {pic.map((e) => (
-          <div
-            key={e}
-            className="relative group cursor-pointer overflow-hidden duration-500  h-80 bg-zinc-800 text-gray-50 p-5 rounded-lg"
-          >
-            <div>
-              <div className="group-hover:scale-110 w-full rounded-lg h-60  duration-500">
-                <Image
-                  src={e}
-                  className="h-full w-full rounded-lg object-cover"
-                  alt=""
-                />
-              </div>
-              <div className="absolute w-full left-0 p-5 -bottom-16 duration-500 group-hover:-translate-y-12">
-                <div className="absolute -z-10 left-0 w-full h-28 opacity-0 duration-500 group-hover:opacity-50 group-hover:bg-blue-900"></div>
-                <span className="text-xl font-bold">Hover me!</span>
-                <p className="group-hover:opacity-100 w-56 duration-500 opacity-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-              </div>
+        {pic.map((e, i) => (
+          <>
+            <div key={e}>
+              <Link href={`/content/aboutus/${i + 1}`}>
+                <div className="relative group cursor-pointer overflow-hidden duration-500  h-80 bg-zinc-800 text-gray-50 p-5 rounded-lg">
+                  <div>
+                    <div className="group-hover:scale-110 w-full rounded-lg h-60  duration-500">
+                      <Image
+                        src={e}
+                        className="h-full w-full rounded-lg object-cover"
+                        alt=""
+                      />
+                    </div>
+                    <div className="absolute w-full left-0 p-5 -bottom-16 duration-500 group-hover:-translate-y-12">
+                      <div className="absolute -z-10 left-0 w-full h-28 opacity-0 duration-500 group-hover:opacity-50 group-hover:bg-blue-900"></div>
+                      <span className="text-xl font-bold">Hover me!</span>
+                      <p className="group-hover:opacity-100 w-56 duration-500 opacity-0">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
             </div>
-          </div>
+          </>
         ))}
       </div>
       <div className="flex justify-center items-center p-4 md:p-2 lg:p-0">
