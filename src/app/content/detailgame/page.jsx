3b3bcb2cf916f4f23119b2d488ec91d685313e06
Nyfong <litongfong12@gmail.com>
2024-11-17 20:13:28 +0700

    firthy eight

--- src/app/content/detailgame/page.jsx
+++ src/app/content/detailgame/page.jsx
@@ -10,7 +10,7 @@ let DetailGame = () => {
       <main className="p-5 md:p-3 lg:p-0  gap-5 md:gap-3 max-w-screen-xl min-w-screen-80 mx-auto my-10">
         {/* <MainProdDetail />
         <SuggestDetailScroll /> */}
-        <div className=" h-[400px] justify-center flex gap-5 items-center justify-center p-5">
+        <div className=" h-[400px] justify-center flex flex-col md:flex-col gap-5 items-center justify-center p-5">
           <p className="text-3xl">hello we are on developing mode</p>
           <DiGit className="text-3xl" />
           <VscVscode className="text-3xl" />
