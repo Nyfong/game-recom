d5f86cd31ecec0f31c005c67b78463938851b24d
Nyfong <litongfong12@gmail.com>
2024-12-23 21:33:40 +0700

    seventy five

--- src/lib/gameData.js
+++ src/lib/gameData.js
@@ -2,17 +2,14 @@ export const get = async () => {
   // Define the URL for local JSON based on the environment
   const url =
     process.env.NODE_ENV === "production"
-      ? "/api/data/data.json" // Use the static path when in production
-      : "http://localhost:3000/api/data/data.json"; // Development mode
+      ? "file://./public/api/data/data.json"
+      : "http://localhost:3000/api/data/data.json";
 
   try {
     let data;
 
-    // If it's server-side in production, use fs to read the local file
-    if (
-      typeof window === "undefined" &&
-      process.env.NODE_ENV === "production"
-    ) {
+    if (process.env.NODE_ENV === "production") {
+      // For production, read the file directly
       const fs = require("fs").promises;
       const path = require("path");
       const filePath = path.join(
@@ -22,28 +19,32 @@ export const get = async () => {
         "data",
         "data.json"
       );
-
-      // Check if the file exists and read it
       const fileContents = await fs.readFile(filePath, "utf8");
       data = JSON.parse(fileContents);
     } else {
-      // For client-side (or development), use fetch
+      // For development, use fetch
       const response = await fetch(url, { cache: "default" });
+
       if (!response.ok) {
         throw new Error(
           `Failed to fetch data. HTTP Status: ${response.status}`
         );
       }
+
       data = await response.json();
     }
 
+    // Ensure the data is an array and contains a 'game' property in the first object
     if (Array.isArray(data) && data[0].game) {
+      // Return the 'game' array from the first object
       return data[0].game.slice(0, 50);
     } else {
       throw new Error("'game' is not an array or missing in the data");
     }
   } catch (error) {
     console.error("Data fetching error:", error);
-    return []; // Return an empty array in case of an error
+
+    // Return an empty array in case of an error to maintain consistency
+    return [];
   }
 };
